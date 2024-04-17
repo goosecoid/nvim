cb14a4bd70fe19f69498f79ed4e3b842d2e34685
goosecoid <adblavier@gmail.com>
2024-04-17 11:23:59 +0200

    fix: copilot

--- init.lua
+++ init.lua
@@ -286,9 +286,9 @@ require('lazy').setup({
     "nvimtools/none-ls.nvim"
   },
 
-  -- {
-  --   "github/copilot.vim"
-  -- },
+  {
+    "github/copilot.vim"
+  },
   -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
   --       These are some example plugins that I've included in the kickstart repository.
   --       Uncomment any of the lines below to enable them.
